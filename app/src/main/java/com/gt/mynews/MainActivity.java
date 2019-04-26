02d799c79759f51cebd2f2f798df00b31e009c77
Shivetya <gthuillier2@gmail.com>
2019-04-26 16:55:50 +0200

    Add Toolbar menu, add bind between main activity and toolbar. Add android icons

--- app/src/main/java/com/gt/mynews/MainActivity.java
+++ app/src/main/java/com/gt/mynews/MainActivity.java
@@ -2,6 +2,8 @@ package com.gt.mynews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.configureToolbar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+        return true;
+    }
+
+    private void configureToolbar(){
+
+        Toolbar toolbar = findViewById(R.id.activity_main_toolbar);
+        setSupportActionBar(toolbar);
     }
 }
