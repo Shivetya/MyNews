0a6b98ae7c3eb307349ff9f677ded09f4fdca331
Shivetya <gthuillier2@gmail.com>
2019-04-29 19:20:04 +0200

    Add fragment PagerFragment

--- app/src/main/java/com/gt/mynews/Activities/MainActivity.java
+++ app/src/main/java/com/gt/mynews/Activities/MainActivity.java
@@ -1,9 +1,11 @@
-package com.gt.mynews;
+package com.gt.mynews.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+
+import com.gt.mynews.R;
 
 public class MainActivity extends AppCompatActivity {
 
