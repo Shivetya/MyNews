ed13f1a43fab0879a105a7e8bcf13032d918702e
Shivetya <gthuillier2@gmail.com>
2019-05-05 17:46:01 +0200

    Add interfaces NytApi and NytUseCase

---
+++ app/src/main/java/com/gt/mynews/usecases/NytApi.kt
@@ -0,0 +1,21 @@
+package com.gt.mynews.usecases
+
+import com.gt.mynews.data.ArticleApiResponse
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface NytApi {
+
+    @GET("/svc/mostpopular/v2/viewed/7.json")
+    fun getMostPopularApi() : Call<ArticleApiResponse>
+
+    @GET("/svc/topstories/v2/home.json")
+    fun getTopStoriesApi() : Call<ArticleApiResponse>
+
+    @GET("/svc/topstories/v2/science.json")
+    fun getScienceApi() : Call<ArticleApiResponse>
+
+    @GET("/svc/topstories/v2/technology.json")
+    fun getTechnologyApi() : Call<ArticleApiResponse>
+}
---
+++ app/src/main/java/com/gt/mynews/usecases/NytUseCase.kt
@@ -0,0 +1,12 @@
+package com.gt.mynews.usecases
+
+import com.gt.mynews.data.ArticleApiResponse
+
+interface NytUseCase{
+
+    fun getMostPopular() : ArticleApiResponse
+    fun getTopStories() : ArticleApiResponse
+    fun getScience() : ArticleApiResponse
+    fun getTechnology() : ArticleApiResponse
+
+}
