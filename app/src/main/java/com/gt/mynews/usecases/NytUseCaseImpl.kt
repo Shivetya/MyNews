47a5a7b8cf89964f7f1aea0873717fb38cf257b9
Shivetya <gthuillier2@gmail.com>
2019-05-05 22:24:58 +0200

    Add NytUseCaseImpl without code

---
+++ app/src/main/java/com/gt/mynews/usecases/NytUseCaseImpl.kt
@@ -0,0 +1,24 @@
+package com.gt.mynews.usecases
+
+import com.gt.mynews.data.ArticleApiResponse
+
+class NytUseCaseImpl : NytUseCase {
+
+
+
+    override fun getMostPopular(): ArticleApiResponse {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getTopStories(): ArticleApiResponse {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getScience(): ArticleApiResponse {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getTechnology(): ArticleApiResponse {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
