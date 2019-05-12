0b321eb86bc8f20da7230ca74c2bd51e653f3977
Shivetya <gthuillier2@gmail.com>
2019-05-12 18:43:57 +0200

    Delete init code in GenericViewModel

--- app/src/main/java/com/gt/mynews/viewmodels/GenericViewModel.kt
+++ app/src/main/java/com/gt/mynews/viewmodels/GenericViewModel.kt
@@ -3,6 +3,7 @@ package com.gt.mynews.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.gt.mynews.data.ArticleApiResponse
 import com.gt.mynews.usecases.NytUseCase
 import kotlinx.coroutines.Job
 
@@ -15,15 +16,6 @@ class GenericViewModel (private val useCase : NytUseCase) : ViewModel() {
     private var currentJob : Job? = null
 
     init {
-        val title = MutableLiveData<String>()
-        val date = MutableLiveData<String>()
-        val description = MutableLiveData<String>()
 
-        title.value = "title"
-        date.value = "11/05/2019"
-        description.value = "description"
-        _mutableListOfLiveData.add(title)
-        _mutableListOfLiveData.add(date)
-        _mutableListOfLiveData.add(description)
     }
 }
