28244695e34ba4d3b967acbc2f91e5bbed84e815
Shivetya <gthuillier2@gmail.com>
2019-06-12 13:48:43 +0200

    Add SearchActivity with no code and activity_search.xml with all views.

---
+++ app/src/main/java/com/gt/mynews/views/activities/SearchActivity.kt
@@ -0,0 +1,13 @@
+package com.gt.mynews.views.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gt.mynews.R
+
+class SearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
