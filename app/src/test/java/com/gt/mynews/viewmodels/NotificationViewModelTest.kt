6ae1d44e95773666c033be2ec23b5acf548ca6ae
Shivetya <gthuillier2@gmail.com>
2019-07-04 18:41:27 +0200

    Correction of bugs with tests in NotificationViewModelTest

--- app/src/test/java/com/gt/mynews/viewmodels/NotificationViewModelTest.kt
+++ app/src/test/java/com/gt/mynews/viewmodels/NotificationViewModelTest.kt
@@ -1,17 +1,17 @@
 package com.gt.mynews.viewmodels
 
+import androidx.test.core.app.ApplicationProvider
 import com.gt.mynews.data.repositories.NotificationSettingsArticlesSaved
 import com.gt.mynews.usecases.NotificationUseCase
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
-import org.robolectric.RuntimeEnvironment
 
 @RunWith(RobolectricTestRunner::class)
 class NotificationViewModelTest {
 
-    private val useCase: NotificationUseCase = NotificationUseCase(NotificationSettingsArticlesSaved(RuntimeEnvironment.application))
+    private val useCase: NotificationUseCase = NotificationUseCase(NotificationSettingsArticlesSaved(ApplicationProvider.getApplicationContext()))
 
     @Test
     fun `should transform keyword and keywordFilter to query ready`(){
